a7f0aadc14396521308f1b2c32c9c630922b4afd
sunnysab <i@sunnysab.cn>
2023-08-07 09:30:10 +0800

    Add bilibi-fav-export

---
+++ bilibili-fav-export/README.md
@@ -0,0 +1,15 @@
+# Bilibi 收藏夹导出脚本
+
+由于 bilibili 经常吞掉视频，并且当视频失效后，无法得知当时收藏的内容，因此编写本脚本导出收藏夹内容，方便未来查看。
+
+本项目基于 [bilibili-API-collect](https://github.com/SocialSisterYi/bilibili-API-collect) 描述的接口开发。
+
+## 依赖
+
+- requests  *发送同步 HTTP 请求*
+
+## 使用
+
+```
+python3 export.py [收藏夹ID]
+```
---
+++ bilibili-fav-export/export.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+
+"""
+API document: https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/fav/list.md
+"""
+
+import requests 
+from typing import List, Any
+from dataclasses import dataclass
+
+
+@dataclass
+class StaredVideo:
+    bv_id: str
+    title: str
+    introduction: str
+    duration: int
+
+
+def export(id: int) -> List[StaredVideo]:
+    def do_map(o: Any) -> StaredVideo:
+        return StaredVideo(bv_id=o['bv_id'],
+                        duration=o['duration'],
+                        introduction='',
+                        title=o['title'])
+
+    def query_by_page(id: int, page: int) -> List[StaredVideo]:
+        response = requests.get('https://api.bilibili.com/x/v3/fav/resource/list', 
+                                params={'media_id': id, 'pn': page, 'ps': 20})
+        response.raise_for_status()
+
+        try:
+            video_list = response.json()['data']['medias'] or []
+        except:
+            print(f'Server returned text: {response.text}')
+            return []
+        
+        return [do_map(e) for e in video_list]
+    
+    result: List[StaredVideo] = []
+    last_size: int = 20
+    last_page: int = 1
+    while last_size == 20:
+        page = query_by_page(id, last_page)
+        result.extend(page)
+
+        last_page += 1
+        last_size = len(page)
+
+    return result
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    assert len(sys.argv) == 2
+
+    fav_collection_id = int(sys.argv[1])
+    exported_collection = export(fav_collection_id)
+
+    filename = f'collection-{fav_collection_id}'
+    count = 0
+    make_filename = lambda: filename + '.csv' if count == 0 else f'{filename}-{count}.csv'
+    while os.path.exists(make_filename()) and count < 10:
+        count += 1
+
+    if count == 10:
+        exit(1)
+
+    with open(make_filename(), 'w+') as f:
+        for v in exported_collection:        
+            l = '\t'.join([str(x) for x in v.__dict__.values()]) + '\n'
+            f.write(l)
