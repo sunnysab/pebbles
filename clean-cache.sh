89fbeb15f4e4bdc6b1a364a58ff8e3f298a5495e
sunnysab <i@sunnysab.cn>
2024-01-26 16:13:46 +0800

    Add some clean path in clean-cache.sh

--- clean-cache.sh
+++ clean-cache.sh
@@ -4,8 +4,17 @@ rm -rf ~/.cache/yay/*
 rm -rf ~/.cache/mozilla/*
 rm -rf ~/.cache/pip/*
 rm -rf ~/.cache/yarn/*
+rm -rf ~/thumbnails/*
 rm -rf ~/go/pkg/mod/cache/download/*
 rm -rf ~/.cargo/registry/cache/*
 rm -rf ~/.gradle/caches/*
+rm -rf ~/.npm/_cacache/*
+rm -rf ~/.debug/*
+rm -rf ~/.rustup/{downloads,tmp}/*
+rm -rf ~/.ghcup/{cache,tmp}/*
+
 
 sudo pacman -Scc
+
+# remove rust build crafts.
+fd -g -s "Cargo.toml" ~/Projects -x sh -c "cd {//}; cargo clean"
