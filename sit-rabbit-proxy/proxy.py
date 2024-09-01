404a272f942d6163ccee5d1c7d0d09797146c1d1
sunnysab <i@sunnysab.cn>
2024-09-01 20:05:23 +0800

    Some fixes but not clearly.
    Maybe `master.addons.add` can accept only one parameter?

--- sit-rabbit-proxy/proxy.py
+++ sit-rabbit-proxy/proxy.py
@@ -1,4 +1,3 @@
-
 from mitmproxy import http
 from mitmproxy.options import Options
 from mitmproxy.tools.dump import DumpMaster
@@ -17,10 +16,11 @@ class IpChanger:
     @staticmethod
     def is_login_request(flow: http.HTTPFlow) -> bool:
         return flow.request.method == 'GET' \
-               and flow.request.pretty_host == 'authserver.sit.edu.cn' \
-               and flow.request.path == '/authserver/login'
+            and flow.request.pretty_host == 'authserver.sit.edu.cn' \
+            and flow.request.path == '/authserver/login'
 
-    def has_been_freezed(self, flow: http.HTTPFlow) -> bool:
+    @staticmethod
+    def has_been_freezed(flow: http.HTTPFlow) -> bool:
         return 'IP被冻结' in flow.response.text
 
     def request(self, flow: http.HTTPFlow):
@@ -29,19 +29,18 @@ class IpChanger:
 
     def response(self, flow: http.HTTPFlow):
         if IpChanger.is_login_request(flow) and flow.response.status_code == 200:
-            if self.has_been_freezed(flow):
+            if IpChanger.has_been_freezed(flow):
                 new_ip = chose_one('eth0', '10.1.160.0/24')
                 change_ip_address('eth0', new_ip)
+                print(f'IP changed to {new_ip}')
             self.lock.release()
 
-addons = [IpChanger()]
+async def run():
+    options = Options(listen_host='0.0.0.0', listen_port=8080, http2=True)
+    master = DumpMaster(options, with_termlog=False, with_dumper=False)
+    master.addons.add(IpChanger())
+    await master.run()
+
 
 if __name__ == '__main__':
-
-    async def run():
-        options = Options(listen_host='0.0.0.0', listen_port=8080, http2=True)
-        master = DumpMaster(options, with_termlog=False, with_dumper=False)
-        master.addons.add(addons)
-        await master.run()
-
     asyncio.run(run())
