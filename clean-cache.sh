0e41760980c363dee468bd7e5b7ddf1399408fa5
sunnysab <i@sunnysab.cn>
2025-05-31 14:41:06 +0800

    fix: 修正清理脚本中的项目路径并新增缓存清理

--- clean-cache.sh
+++ clean-cache.sh
@@ -12,13 +12,14 @@ rm -rf ~/.npm/_cacache/*
 rm -rf ~/.debug/*
 rm -rf ~/.rustup/{downloads,tmp}/*
 rm -rf ~/.ghcup/{cache,tmp}/*
+rm -rf ~/.cache/thumbnails/*
 
 sudo pacman -Scc
 
 # remove rust build crafts.
 if [ -e ~/.cargo/bin/cargo-clean-recursive ]; then
-    cargo clean-recursive ~/Projects/;
+    cargo clean-recursive ~/Code/;
 else
     echo "please consider to install cargo-clean-recursive."
-    fd -g -s "Cargo.toml" ~/Projects -x sh -c "cd {//}; cargo clean";
+    fd -g -s "Cargo.toml" ~/Code -x sh -c "cd {//}; cargo clean";
 fi
